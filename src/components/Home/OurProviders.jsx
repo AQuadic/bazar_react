34ceabc554465af279d85022e569e23ba6e5a7b4
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-05-29 14:18:51 +0300

    Change nav logo add heart

--- src/components/Home/OurProviders.jsx
+++ src/components/Home/OurProviders.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router"
 import AdsStore from '/images/Home/AdsStore.png'
 import product1 from '/images/Home/product1.png'
 import product2 from '/images/Home/product2.png'
+import Heart from "../icons/Home/Heart";
 const categories = [
     { id: 1, title: 'ADS Store', img: AdsStore, description: 'Lorem ipsum dolor sit amet consectetur. Sagittis cursus dictum mattis condimentum risus.' },
     { id: 2, title: 'ADS Store', img: AdsStore, description: 'Lorem ipsum dolor sit amet consectetur. Sagittis cursus dictum mattis condimentum risus.' },
@@ -17,11 +18,14 @@ const OurProviders = () => {
                 <Link to='/' className="text-[#101010] text-2xl font-medium">See All</Link>
             </div>
 
-            <div className="flex flex-wrap items-center justify-between mt-8">
+            <div className="flex flex-wrap items-center sm:justify-between justify-center mt-8">
                 {categories.map(({ id, title, img, description }) => (
-                    <div key={id} className="flex flex-col mt-8 ">
+                    <div key={id} className="flex flex-col mt-8 relative">
                         <img src={img} alt={title} className="w-[100px] h-[100px] mx-auto" />
-                        <h2 className="text-[#101010] text-xl font-semibold text-center mt-6">{title}</h2>
+                        <div className="absolute top-24 right-0">
+                            <Heart />
+                        </div>
+                        <h2 className="text-[#101010] text-xl font-semibold text-center mt-14">{title}</h2>
                         <p className="w-[220px] text-[#101010] text-sm font-medium text-center mt-3">{description}</p>
                         <div>
                             <h3 className="text-[#101010] text-xl font-semibold mt-4">Products</h3>
